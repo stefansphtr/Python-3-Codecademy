d31e0168590301f1609ea305d3ea747689992b83
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2024-01-09 11:14:01 +0700

    refactor(scrabble.py): formatting the code to improve readabilty

--- mini-project/scrabble.py
+++ mini-project/scrabble.py
@@ -1,10 +1,64 @@
-letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+letters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
+points = [
+    1,
+    3,
+    3,
+    2,
+    1,
+    4,
+    2,
+    4,
+    1,
+    8,
+    5,
+    1,
+    3,
+    4,
+    1,
+    3,
+    10,
+    1,
+    1,
+    1,
+    1,
+    4,
+    4,
+    8,
+    4,
+    10,
+]
 
 """Build your Point Dictionary"""
 
 # Task 1
-letter_to_points = {letter:point for letter, point in zip(letters, points)}
+letter_to_points = {letter: point for letter, point in zip(letters, points)}
 print(letter_to_points)
 
 # Task 2
@@ -14,13 +68,15 @@ print(letter_to_points)
 
 """Score a Word"""
 
+
 # Task 3 - 6
 # Given the list of word, I want you to sum the score of each letter inside the word
 def score_word(word):
-  point_total = 0
-  for letters in word:
-    point_total += letter_to_points.get(letters, 0)
-  return point_total
+    point_total = 0
+    for letters in word:
+        point_total += letter_to_points.get(letters, 0)
+    return point_total
+
 
 # Test the function score_word in a list
 print("\n Score word of Stefan's name: \n")
@@ -35,17 +91,22 @@ print(brownie_points)
 """Score a Game"""
 
 # Task 9
-player_to_words = {"player1":["BLUE", "TENNIS", "EXIT"], "wordNerd": ["EARTH", "EYES", "MACHINE"], "Lexi Con": ["ERASER", "BELLY", "HUSKY"], "Prof Reader": ["ZAP", "COMA", "PERIOD"]}
+player_to_words = {
+    "player1": ["BLUE", "TENNIS", "EXIT"],
+    "wordNerd": ["EARTH", "EYES", "MACHINE"],
+    "Lexi Con": ["ERASER", "BELLY", "HUSKY"],
+    "Prof Reader": ["ZAP", "COMA", "PERIOD"],
+}
 
 # Task 10
 player_to_points = {}
 
 # Task 11 - 13
 for player, words in player_to_words.items():
-  player_points = 0
-  for word in words:
-    player_points += score_word(word)
-  player_to_points[player] = player_points
+    player_points = 0
+    for word in words:
+        player_points += score_word(word)
+    player_to_points[player] = player_points
 
 # Task 14
 print("\n Score word of player wordNerd: \n")
@@ -58,11 +119,13 @@ print(player_to_points["wordNerd"])
 
 # Create a function play_world() that would take in a player and word, and add that word to the list of words they've played
 
+
 def play_word(player, word):
-  if player in player_to_words.keys():
-    player_to_words[player].append(word)
-  else:
-    player_to_words[player] = [word]
+    if player in player_to_words.keys():
+        player_to_words[player].append(word)
+    else:
+        player_to_words[player] = [word]
+
 
 print("\n This is the list of words from player1 after add the word DRAGUNOV: \n")
 
@@ -74,12 +137,14 @@ print(player_to_words["player1"])
 print("\n This is the dictionary of player_to_words: \n")
 print(player_to_words)
 
+
 def update_point_totals():
-  for player, words in player_to_words.items():
-    player_points = 0
-    for word in words:
-      player_points += score_word(word)
-    player_to_points[player] = player_points
+    for player, words in player_to_words.items():
+        player_points = 0
+        for word in words:
+            player_points += score_word(word)
+        player_to_points[player] = player_points
+
 
 update_point_totals()
 
@@ -89,8 +154,10 @@ print(player_to_points)
 
 # Make the letter_to_points dictionary able to handle lowercase inputs as well
 
-letter_to_points = {letter.upper():point for letter, point in zip(letters, points)}
-letter_to_points.update({letter.lower():point for letter, point in zip(letters, points)})
+letter_to_points = {letter.upper(): point for letter, point in zip(letters, points)}
+letter_to_points.update(
+    {letter.lower(): point for letter, point in zip(letters, points)}
+)
 letter_to_points[" "] = 0
 
 # Test to soring the lowercase letter:
